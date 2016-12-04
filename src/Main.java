7807dfda49c8e0e81d80067662fc835affd38b27
Cristof17 <cristof_rotsching@yahoo.com>
2016-12-04 21:17:22 +0200

    Tried tests with padding for objects

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main implements Runnable{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			FalseSharing.main(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void run() {
+
+		
+	}
+
+}
